d1bf5d5fb7661da4e6fefc4f617156214471d91c
Clayton Watts <cletusw@gmail.com>
2014-02-06 16:08:37 -0700

    Sunburst directive

--- source/js/Main/main-controller.js
+++ source/js/Main/main-controller.js
@@ -1,6 +1,9 @@
 'use strict';
 
 angular.module('ztop')
-  .controller('MainCtrl', function($scope) {
+  .controller('MainCtrl', function($scope, $http) {
     $scope.hi = 'howdy';
+    $http.get('flare.json').then(function(response) {
+      $scope.data = response.data;
+    });
   });
