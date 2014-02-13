5589ddb4e9daeb82c2cdb8281cae8bd3773cb5cd
Clayton Watts <cletusw@gmail.com>
2014-02-13 16:02:03 -0700

    Remove Main route

--- source/js/Main/main-controller.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('ztop')
-  .controller('MainCtrl', function($scope, webSocketConnection) {
-    $scope.hi = 'howdy';
-    webSocketConnection.subscribe('flare', function(data) {
-      $scope.data = data;
-    });
-  });
